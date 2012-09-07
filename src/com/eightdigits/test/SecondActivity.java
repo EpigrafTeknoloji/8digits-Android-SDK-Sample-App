73597c3d25d09852b81a6ef5eb281eb115e176e3
Gurkan Oluc <gurkanoluc@gmail.com>
2012-09-07 04:16:26 +0300

    Hit Create and Event sending added to SecondActivity

--- src/com/eightdigits/test/SecondActivity.java
+++ src/com/eightdigits/test/SecondActivity.java
@@ -1,20 +1,34 @@
 package com.eightdigits.test;
 
+import com.eightdigits.sdk.EightDigitsClient;
+import com.eightdigits.sdk.exceptions.EightDigitsApiException;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class SecondActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
-    }
+  EightDigitsClient eightDigitsClient;
+  String            hitCode;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_second, menu);
-        return true;
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_second);
+    this.eightDigitsClient = EightDigitsClient.getInstance();
+    try {
+      this.hitCode = this.eightDigitsClient.newScreen("SecondAndroidActivity",
+          "/second");
+    } catch (EightDigitsApiException e) {
+      Log.e("API Error", e.getMessage());
     }
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.activity_second, menu);
+    return true;
+  }
 }
